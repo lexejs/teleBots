da63480fc8aaa9a84b9c024a50fd24dc5b2786fb
Alexei Siuzev <lexejs@gmail.com>
2023-11-30 22:39:32 +0100

    Create credentials.secrets.js

---
+++ credentials.secrets.js
@@ -0,0 +1,8 @@
+credentials = {
+    ubersetzenAssistId: process.env["ubersetzenAssistId"],
+    openAIKey: process.env["openAIKey"],
+    allowedUsers: process.env["allowedUsers"],
+    telegramToken: process.env["telegramToken"],
+};
+
+module.exports = credentials;
